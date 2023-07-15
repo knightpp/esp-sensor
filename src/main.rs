c8b9147010eae7884b744f20564c44b8b5d657ff
Danylo Kondratiev <knightpp@proton.me>
2023-07-15 19:59:45 +0300

    Works

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc, IO};
+
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
@@ -27,7 +28,44 @@ fn main() -> ! {
     rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
-    println!("Hello world!");
 
-    loop {}
+    let mut delay = hal::Delay::new(&clocks);
+    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut data_pin = io.pins.gpio15.into_open_drain_output();
+
+    let mut clk = io.pins.gpio12.into_open_drain_output();
+    let mut dio = io.pins.gpio13.into_open_drain_output();
+    let mut screen_delay = delay.clone();
+    let mut tm = tm1637::TM1637::new(&mut clk, &mut dio, &mut screen_delay);
+    tm.init().unwrap();
+    tm.clear().unwrap();
+    tm.set_brightness(255).unwrap();
+
+    loop {
+        let value =
+            match dht_hal_drv::dht_read(dht_hal_drv::DhtType::DHT22, &mut data_pin, &mut |d| {
+                delay.delay_us(d);
+            }) {
+                Ok(value) => value,
+                Err(err) => {
+                    println!("Error: {:?}", err);
+                    delay.delay_ms(2000u16);
+                    continue;
+                }
+            };
+
+        let temp = value.temperature();
+        let hum = value.humidity();
+        println!("Temperature: {:2.1}°C", temp);
+        println!("Humidity:    {:2.1}%", hum);
+
+        let digits = [
+            ((temp / 10.) as u32 % 10) as u8,
+            (temp as u32 % 10) as u8,
+            ((hum / 10.) as u32 % 10) as u8,
+            (hum as u32 % 10) as u8,
+        ];
+        tm.print_hex(0, &digits).unwrap();
+        delay.delay_ms(5000u16);
+    }
 }
